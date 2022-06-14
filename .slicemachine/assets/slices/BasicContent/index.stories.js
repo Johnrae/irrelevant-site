a1fceadae946a8e56a57fffeff785c8285a63207
John Rae <john.rae23@gmail.com>
2022-06-14 19:09:48 -0400

    Build events pages and add more slices

---
+++ .slicemachine/assets/slices/BasicContent/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/BasicContent';
+
+export default {
+  title: 'slices/BasicContent'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"basic_content","items":[],"primary":{"body":[{"type":"paragraph","text":"Mollit fugiat eu officia sunt ex deserunt sunt aliquip nulla veniam consectetur sit mollit proident ipsum.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
