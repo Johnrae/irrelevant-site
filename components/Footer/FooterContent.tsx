c8efd76d0bc5ab0e8dc0a3e0d799f521d12e2fe5
John Rae <john.rae23@gmail.com>
2022-06-23 19:43:46 -0400

    Idk

--- components/Footer/FooterContent.tsx
+++ components/Footer/FooterContent.tsx
@@ -1,19 +1,19 @@
 export default function FooterContent() {
   return (
     <>
-      <div className='col-span-5 flex flex-col'>
+      <div className='col-span-8 md:col-span-4 flex flex-col'>
         <span>Irrelevant</span>
         <span>©2022</span>
       </div>
-      <div className='col-span-1 flex flex-col'>
+      <div className='col-span-4 md:col-span-2 flex flex-col'>
         <span>Kyle Swick</span>
         <span>kyle@irrelevantmusic.net</span>
       </div>
-      <div className='col-span-1 flex flex-col'>
+      <div className='col-span-2 md:col-span-1 flex flex-col'>
         <span>design by</span>
         <span>Andrew Walsh</span>
       </div>
-      <div className='col-span-1 flex flex-col'>
+      <div className='col-span-2 md:col-span-1 flex flex-col'>
         <span>development by</span>
         <span>John Rae</span>
       </div>
