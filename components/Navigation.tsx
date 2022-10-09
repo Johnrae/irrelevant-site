1782bab42ff8542f909b2a692d9cd165ae298a83
John Rae <john.rae23@gmail.com>
2022-10-09 17:17:00 -0400

    Add mobile navbar

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,6 +1,23 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
+const Links = () => (
+  <>
+    <Link href={'/events'}>Events</Link>
+    <Link href={'/bands'}>Roster</Link>
+    <a
+      target='_blank'
+      rel='noreferrer'
+      href={'https://irrelevantmusic.net/shop'}
+    >
+      Shop
+    </a>
+  </>
+)
 export default function Navigation() {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+
   return (
     <>
       <nav className='difference fixed w-full flex flex-row justify-between items-center px-8 py-4 z-50 text-xl'>
@@ -8,18 +25,46 @@ export default function Navigation() {
           <Link href={'/'}>Irrelevant</Link>
         </div>
 
-        <div className='flex flex-row space-x-4'>
-          <Link href={'/events'}>Events</Link>
-          <Link href={'/bands'}>Roster</Link>
-          <a
-            target='_blank'
-            rel='noreferrer'
-            href={'https://irrelevantmusic.net/shop'}
-          >
-            Shop
-          </a>
+        <div className='hidden md:flex flex-row space-x-8'>
+          <Links />
+        </div>
+
+        <div className='md:hidden'>
+          <button onClick={toggle}>
+            <span>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+                />
+              </svg>
+            </span>
+          </button>
         </div>
       </nav>
+
+      {isOpen && (
+        <div className='md:hidden fixed top-0 left-0 w-full h-full bg-white z-50'>
+          <div className='flex flex-col justify-center items-center h-full relative space-y-4'>
+            <Link href={'/'}>Home</Link>
+            <Links />
+          </div>
+          <button
+            className='fixed top-0 right-0 px-8 py-4 cursor-pointer'
+            onClick={toggle}
+          >
+            <span>Close</span>
+          </button>
+        </div>
+      )}
     </>
   )
 }
