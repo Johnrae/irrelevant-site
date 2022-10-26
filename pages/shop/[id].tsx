149116c0af1e50e160f0931d9ceea27bdb1b5077
John Rae <john.rae23@gmail.com>
2022-10-26 00:18:56 -0400

    Move currency formatter to shared file

--- pages/shop/[id].tsx
+++ pages/shop/[id].tsx
@@ -1,17 +1,9 @@
 import { useFillScreen } from '../../hooks/useFillScreen'
 import { getAllProducts, getProduct } from '../../lib/shopify'
+import { formatter } from '../../lib/formatter'
 
 export default function ProductPage({ product }: any) {
   const { minHeight } = useFillScreen()
-
-  var formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-
-    // These options are needed to round to whole numbers if that's what you want.
-    minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-  })
 
   const price = formatter.format(product.priceRange?.minVariantPrice?.amount)
 
--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,16 +1,9 @@
 import Link from 'next/link'
 import { useFillScreen } from '../../hooks/useFillScreen'
 import { shopifyClient } from '../../lib/shopify'
+import { formatter } from '../../lib/formatter'
 
 function ProductCard({ product }: { product: any }) {
-  var formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-
-    // These options are needed to round to whole numbers if that's what you want.
-    minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-  })
 
   const price = formatter.format(product.priceRange?.minVariantPrice?.amount)
 
