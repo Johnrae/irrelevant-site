e1f13dbcf61e8dc2523b9da172d0a02f6742a0ca
John Rae <john.rae23@gmail.com>
2022-10-26 00:08:07 -0400

    Set min/max fraction digit in currencty formatter

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -8,8 +8,8 @@ function ProductCard({ product }: { product: any }) {
     currency: 'USD',
 
     // These options are needed to round to whole numbers if that's what you want.
-    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+    maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
   })
 
   const price = formatter.format(product.priceRange?.minVariantPrice?.amount)
